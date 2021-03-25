bb1afa09d63045e34b7893a2a1b13ade9437e6f1
Saam J. Z3R0 <bsef18m038@pucit.edu.pk>
2021-03-25 21:10:57 +0500

    Listviews

---
+++ ListViews/app/src/main/java/com/example/listviews/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.listviews;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class MainActivity extends AppCompatActivity {
+
+
+     @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
