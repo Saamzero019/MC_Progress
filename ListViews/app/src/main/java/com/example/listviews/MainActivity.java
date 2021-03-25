ec371e84f1d901b865189fc1b8d846fc2be0e67f
Saam J. Z3R0 <bsef18m038@pucit.edu.pk>
2021-03-25 21:49:34 +0500

    java Code

--- ListViews/app/src/main/java/com/example/listviews/MainActivity.java
+++ ListViews/app/src/main/java/com/example/listviews/MainActivity.java
@@ -14,9 +14,35 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class MainActivity extends AppCompatActivity {
-     @Override
+     ArrayList<String> namesList;
+    ArrayAdapter<String> arrayAdapter;
+    ListView listView;
+    EditText editText;
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        listView=findViewById(R.id.namesListView);
+        editText=findViewById(R.id.fieldNameText);
+        namesList=new ArrayList<String>();
+        namesList.add("Bravo");
+        namesList.add("Charlie");
+        namesList.add("Khan");
+        namesList.add("test");
+        arrayAdapter= new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,namesList);
+        listView.setAdapter(arrayAdapter);
+
+    }
+
+    public void addNameFunc(View view) {
+        String name=editText.getText().toString();
+        name=name.trim();
+        if(!name.isEmpty())
+        {
+            namesList.add(name);
+            Collections.sort(namesList);
+            arrayAdapter.notifyDataSetChanged();
+            editText.setText("");
+        }
     }
 }
