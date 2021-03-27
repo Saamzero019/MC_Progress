6816a2b0612f1388a5d38a97648f734ebeff378c
Saam J. Z3R0 <bsef18m038@pucit.edu.pk>
2021-03-28 03:28:01 +0500

    Seekbar

---
+++ Seekbar/app/src/main/java/com/example/seekbar/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.seekbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SeekBar;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    //TODO: seekbar
+    }
