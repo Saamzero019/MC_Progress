4886d6327760c3643be656f877e38665a76020c5
Saam J. Z3R0 <bsef18m038@pucit.edu.pk>
2021-03-28 18:42:26 +0500

    Animation

--- Animation/app/src/main/java/com/example/animation/MainActivity.java
+++ Animation/app/src/main/java/com/example/animation/MainActivity.java
@@ -14,5 +14,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-   
+    public void animate(View view) {
+        ImageView imageView=findViewById(R.id.imageView);
+        //imageView.animate().translationXBy(-500).setDuration(1000);
+        //imageView.animate().translationXBy(500).setDuration(1000);
+        //imageView.animate().rotation(360).alpha(0).setDuration(1000);
+        //imageView.animate().scaleX(2f);
+        //imageView.animate().scaleY(2f);
+        imageView.animate().scaleX(1.5f).scaleY(1.5f).rotation(360).setDuration(2000);
+    }
 }
