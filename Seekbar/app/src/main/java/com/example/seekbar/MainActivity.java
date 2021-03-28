285d3d16d0cf1b5fea1a940de496c4d10222da7c
Saam J. Z3R0 <bsef18m038@pucit.edu.pk>
2021-03-28 11:05:48 +0500

    Seekbar

--- Seekbar/app/src/main/java/com/example/seekbar/MainActivity.java
+++ Seekbar/app/src/main/java/com/example/seekbar/MainActivity.java
@@ -14,5 +14,36 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    //TODO: seekbar
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        SeekBar seekBar=findViewById(R.id.seekBar);
+        seekBar.setMax(40);
+        ListView listView=findViewById(R.id.listView);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int index, boolean fromUser) {
+                int tableNumber=index;
+                ArrayList<String> list=new ArrayList<String>();
+                for(int x=1;x<11;x++){
+                    list.add(Integer.toString(x*index));
+                }
+                ArrayAdapter<String>arrayAdapter=new ArrayAdapter<String>(getApplicationContext(),android.R.layout.simple_list_item_1,list);
+                listView.setAdapter(arrayAdapter);
+            }
+
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
     }
