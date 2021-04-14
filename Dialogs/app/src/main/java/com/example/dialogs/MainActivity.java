5a495b50dd608dd66bf9892c6aea70630fb98bdb
Saam J. Z3R0 <bsef18m038@pucit.edu.pk>
2021-04-14 16:18:01 +0500

    Dialog

---
+++ Dialogs/app/src/main/java/com/example/dialogs/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.dialog;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button=findViewById(R.id.btnDialog);
+    }
+
+    public void ShowDialog(View view) {
+     
+    }
+}
